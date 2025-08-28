06c4f62b3590c767117bcc3faf563b2616bbb0be
脸肿的厉害 <1187246802@qq.com>
2025-08-29 01:45:39 +0800

    测试文件

---
+++ tdTest/tdTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace tdTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Console.WriteLine(123);
+    }
+}
