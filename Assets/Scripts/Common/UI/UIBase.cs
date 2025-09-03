c2086ea17bfc281fb02a7d24adb05c7149a09a7d
脸肿的厉害 <1187246802@qq.com>
2025-09-04 00:44:18 +0800

    提交

--- Assets/Scripts/Common/UI/UIBase.cs
+++ Assets/Scripts/Common/UI/UIBase.cs
@@ -8,6 +8,72 @@ namespace Common.UI
 {
     public abstract class UIBase : MonoBehaviour
     {
+        public bool isInitialized = false;
+        public bool isVisible = false;
         
+        #region 生命周期
+        
+        protected virtual void Awake()
+        {
+            Initialize();
+        }
+        
+        protected virtual void OnDestroy()
+        {
+            Cleanup();
+        }
+        
+        #endregion
+        
+        #region 初始化
+        
+        protected virtual void Initialize()
+        {
+            if (isInitialized) return;
+            
+            InitUI();
+            isInitialized = true;
+        }
+        
+        protected virtual void InitUI()
+        {
+            // 子类可以重写此方法进行UI初始化
+        }
+        
+        #endregion
+        
+        #region 显示控制
+        public virtual void Show()
+        {
+            if (!isInitialized)
+            {
+                Initialize();
+            }
+            
+            gameObject.SetActive(true);
+            isVisible = true;
+        }
+        
+        public virtual void Hide()
+        {
+            gameObject.SetActive(false);
+            isVisible = false;
+        }
+        
+        public virtual void Destroy()
+        {
+            Destroy(gameObject);
+        }
+        
+        #endregion
+        
+        #region 清理
+        
+        protected virtual void Cleanup()
+        {
+            // 子类可以重写此方法进行清理工作
+        }
+        
+        #endregion
     }
 } 
--- Assets/Scripts/Operator/WndOperatorList.cs
+++ Assets/Scripts/Operator/WndOperatorList.cs
@@ -10,8 +10,16 @@ namespace Operator
     /// 干员列表主窗口
     /// 管理干员列表界面的整体逻辑
     /// </summary>
-    public class WndOperatorList : WndBase
+    public class WndOperatorList:WndBase
     {
+        protected override void InitUI()
+        {
+            throw new System.NotImplementedException();
+        }
         
+        protected override void Awake()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
